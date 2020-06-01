77adc5c0a8dc32d6196c6315618a7b113d2869d3
Matt Gordon <bigmattsize21@gmail.com>
2020-06-01 00:22:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,13 +9,13 @@ class App < Sinatra::Base
   
   get '/square/:number' do
     @num = params[:number].to_i
-    erb :square
+    @num.square
   end
   
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    erb :say
+    
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
