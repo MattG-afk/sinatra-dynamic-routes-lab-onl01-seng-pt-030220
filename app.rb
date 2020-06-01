d539d0fb142b02dbba4026fa07a550ced03a2ddb
Matt Gordon <bigmattsize21@gmail.com>
2020-06-01 00:06:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,22 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/reversename/:name'
+  get '/reversename/:name' do
     @name = params[:name]
     erb :reversename
   end
   
-  get '/square/:number'
+  get '/square/:number' do
     @num = params[:number].to_i
     erb :square
   end
   
-  get '/say/:number/:phrase'
+  get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
   end
   
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
@@ -26,7 +26,7 @@ class App < Sinatra::Base
   erb :saywords
   end
   
-  get '/:operation/:number1/:number2'
+  get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
