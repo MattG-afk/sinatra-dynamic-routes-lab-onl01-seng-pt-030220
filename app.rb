1d2632e0d8e6b23f77a5e54cdd38471774e1efc9
Matt Gordon <bigmattsize21@gmail.com>
2020-06-01 00:01:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5'
   @word1 = params[:word1]
   @word2 = params[:word2]
-  @word3 
-  @word4
+  @word3 = params[:word1]
+  @word4 
   @word5
   erb :saywords
   end
